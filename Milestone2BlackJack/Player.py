35d7b14bfa3d6ac40d1ffb4b819c1901c115dd2f
Joey Francisco <joey.francisco@ext.essilor.com>
2025-06-27 17:30:24 +0800

    added bankroll initialization and deduction methods, added bankroll initialization and deduction unittest

--- Milestone2BlackJack/Player.py
+++ Milestone2BlackJack/Player.py
@@ -1,7 +1,21 @@
 class Player:
 
+    bankroll = 0.0
+
     def __init__(self, name):
         self.name = name
 
     def __str__(self):
         return f'I am Player {self.name}'
+
+    def initialize_bankroll(self, starting_amt):
+        self.bankroll = starting_amt
+
+    def bankroll_deduct(self, amount):
+        try:
+            self.bankroll -= amount
+        except:
+            print(f'Cannot deduct the specified amount on {self.name}\'s bankroll')
+            return False
+        else:
+            return self.bankroll
--- Milestone2BlackJack/unitTest/PlayerTest.py
+++ Milestone2BlackJack/unitTest/PlayerTest.py
@@ -1,12 +1,14 @@
+import copy
 import unittest
 from Milestone2BlackJack.Player import Player
-
 
 class MyTestCase(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
+        initial_amt = 100.0
         cls.my_player = Player('Joey')
+        cls.my_player.initialize_bankroll(initial_amt)
 
     def testPlayerName(self):
         expected_name = 'Joey'
@@ -15,6 +17,16 @@ class MyTestCase(unittest.TestCase):
     def testPlayerClassStr(self):
         expected = 'I am Player Joey'
         self.assertEqual(expected, str(self.my_player))
+
+    def test_initialize_bankroll(self):
+        expected = 100.0
+        self.assertEqual(expected, self.my_player.bankroll)
+
+    def testBankrollDeduction(self):
+        expected = 80.0
+        deduction_amt = 20.0
+        my_player_copy = copy.deepcopy(self.my_player)
+        self.assertEqual(expected, my_player_copy.bankroll_deduct(deduction_amt))
 
 if __name__ == '__main__':
     unittest.main()
